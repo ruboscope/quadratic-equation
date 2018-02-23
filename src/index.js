33cfccef3a5ac225e22f58e32aa0ba9acbcf4758
Zhukov Andrey <AS.Zhukow@BelVeb.by>
2018-02-23 14:36:52 +0300

    -решил уравнение через теорему Виета. Округлил до целого. Сортировка

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  var equationArray = equation.split(' ');
+  var quadrCoef = equationArray[0];
+  var linearCoef = equationArray[3] + equationArray[4];
+  var freeCoef = equationArray[equationArray.length - 2] + equationArray[equationArray.length-1];
+  var sqrtDiscr = Math.sqrt(linearCoef * linearCoef - 4 * quadrCoef * freeCoef);
+  var results = [Math.round(((-1) * linearCoef + sqrtDiscr) / (2 * quadrCoef)), Math.round(((-1) * linearCoef - sqrtDiscr) / (2 * quadrCoef))];
+  return results.sort(function (a, b) {
+    return a - b;
+  }); 
 }
